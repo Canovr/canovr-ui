a70fbdff05d573515847f445a1662da90d7e6b20
symb-int <apps@symbolic-intelligence.de>
2026-03-19 15:26:43 +0100

    Ich bin dein ergebener Diener! Register app fonts and fix Xcode root路径

--- CanovrApp.swift
+++ CanovrApp.swift
@@ -1,8 +1,13 @@
 import SwiftUI
+import CoreText
 
 @main
 struct CanovRApp: App {
     @State private var appState = AppState()
+
+    init() {
+        FontRegistrar.registerAppFonts()
+    }
 
     var body: some Scene {
         WindowGroup {
@@ -33,6 +38,47 @@ struct CanovRApp: App {
                 }
             }
             .environment(appState)
+        }
+    }
+}
+
+private enum FontRegistrar {
+    // Registriert die Font-Dateien beim Start explizit im Prozess.
+    private static let appFontFiles = [
+        "FugazOne-Regular.ttf",
+        "Lato-Bold.ttf",
+        "Lato-Light.ttf",
+        "Lato-Regular.ttf",
+    ]
+
+    static func registerAppFonts() {
+        for fileName in appFontFiles {
+            registerFont(named: fileName)
+        }
+    }
+
+    private static func registerFont(named fileName: String) {
+        guard let fontURL = Bundle.main.url(forResource: fileName, withExtension: nil) else {
+            #if DEBUG
+            print("Font-Datei nicht im Bundle gefunden: \(fileName)")
+            #endif
+            return
+        }
+
+        var registrationError: Unmanaged<CFError>?
+        let didRegister = CTFontManagerRegisterFontsForURL(fontURL as CFURL, .process, &registrationError)
+        guard !didRegister else { return }
+
+        if let registrationError {
+            let nsError = registrationError.takeRetainedValue() as Error as NSError
+            if nsError.domain == kCTFontManagerErrorDomain as String,
+               nsError.code == CTFontManagerError.alreadyRegistered.rawValue {
+                return
+            }
+
+            #if DEBUG
+            print("Font-Registrierung fehlgeschlagen (\(fileName)): \(nsError.localizedDescription)")
+            #endif
         }
     }
 }
